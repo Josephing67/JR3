6664f3af2d688d7dc4e9fc658f0b4ab49d8561d0
Josephing67 <80915563+Josephing67@users.noreply.github.com>
2021-05-20 17:49:29 -0600

    Homework

---
+++ JS-Basics-2-Homework/index.js
@@ -0,0 +1,56 @@
+function grade() {
+gradeAPlus = 97,     
+gradeA = 93,
+gradeAMinus = 90,
+gradeBPlus = 87,     
+gradeB = 83,
+gradeBMinus = 80,
+gradeCPlus = 77,     
+gradeC = 73,
+gradeCMinus = 70,
+gradeDPlus = 67,     
+gradeD = 63,
+gradeDMinus = 60,
+gradeF = 0,
+studentGrade = 97;
+if (studentGrade >= gradeAPlus) {
+    console.log("A+");
+} 
+else if  (studentGrade >= gradeA) {
+     console.log("A");   
+}
+else if (studentGrade >= gradeAMinus) {
+     console.log("A-");   
+}
+else if (studentGrade >= gradeBPlus) {
+     console.log("B+");   
+}
+else if (studentGrade >= gradeB) {
+     console.log("B");   
+}
+else if  (studentGrade >= gradeBMinus) {
+     console.log("B-");   
+}
+else if (studentGrade >= gradeCPlus) {
+     console.log("C+");   
+}
+else if (studentGrade >= gradeC) {
+     console.log("C");   
+}
+else if (studentGrade >= gradeCMinus) {
+     console.log("C-");   
+}
+else if (studentGrade >= gradeDPlus) {
+     console.log("D+");   
+}
+else if (studentGrade >= gradeD) {
+     console.log("D");   
+}
+else if (studentGrade >= gradeDMinus) {
+     console.log("D-");   
+}
+else if (studentGrade >= gradeF) {
+     console.log("F");   
+}
+}
+grade()
